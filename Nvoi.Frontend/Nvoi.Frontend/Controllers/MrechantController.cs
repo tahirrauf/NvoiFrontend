8e9e8c0a2a380e615095f89a86cbe483d2b0047c
Tahir Rauf <tahir.abdul.rauf@gmail.com>
2017-03-12 01:01:39 +0000

    Project Foundational Sturucture commit with Bootstrap, Angular, Jquery and Modernizer

---
+++ Nvoi.Frontend/Nvoi.Frontend/Controllers/MrechantController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nvoi.Frontend.Controllers
+{
+    public class MrechantController : Controller
+    {
+        // GET: Mrechant
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Nvoi.Frontend/Nvoi.Frontend/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nvoi.Frontend.MvcApplication" Language="C#" %>
